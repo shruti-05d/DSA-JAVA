7c7f4656e232d0fac2f4924f1424d64ca18d49cb
Shruti Dhole <doshru05@gmail.com>
2025-08-06 23:22:37 +0530

    Add LargestOf2 if-else example

---
+++ conditionalstatements/if-else/largestoftwo.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class largestoftwo{
+
+  public static void main(String args[]){
+
+    int A=1;
+    int B=5;
+    if(A>=B){
+       System.out.println("A is largest");
+    }else{
+      System.out.println("B is largest");
+    }
+  }
+}
