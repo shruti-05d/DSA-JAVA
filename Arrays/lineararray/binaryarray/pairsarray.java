f2bca61b848cbdd29028e5cfa69c4d8910bca083
Shruti Dhole <doshru05@gmail.com>
2025-08-09 14:30:40 +0530

    pairs of element

---
+++ Arrays/lineararray/binaryarray/pairsarray.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class pairsarray{
+    public static void pairs(int numbers[]){
+        int tp=0;
+        for(int i=0;i<numbers.length;i++){
+            int curr=numbers[i];
+
+            for(int j=0;j<numbers.length;j++){
+                System.out.println("("+curr+","+numbers[j]+")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("total pairs:"+tp);
+    }
+    public static void main(String args[]){
+        int numbers[]={1,3,5,7,9,11,13,15,17};
+
+    pairs(numbers);
+    }
+}
