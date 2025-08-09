1b80860b00094111636488e770b1821e8ab80dca
Shruti Dhole <doshru05@gmail.com>
2025-08-09 13:49:52 +0530

    reverse array

---
+++ Arrays/lineararray/binaryarray/reversearray.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class reversearray{
+public static void reverse(int numbers[]){
+    int first=0,last=numbers.length-1;
+
+    while(first<last){
+        int temp=numbers[last];
+        numbers[last]=numbers[first];
+        numbers[first]=temp;
+        first++;
+        last--;
+    }
+}
+    public static void main(String args[]){
+        int numbers[]={1,3,5,7,9,11,13,15};
+        reverse(numbers);
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]+"");
+        }
+        System.out.println();
+
+    }
+}
